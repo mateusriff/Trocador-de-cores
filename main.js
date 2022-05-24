1173da20fba0dbe725c26d0af81da17aa69c0de2
mateusriff <mateusnriff@gmail.com>
2022-05-24 16:18:08 -0300

    change main.js

--- main.js
+++ main.js
@@ -2,10 +2,13 @@
 
 function changecolor (){
     if(document.body.style.background = '#292929'){
-         document.body.style.background = '#F2F2F2';
+        document.body.style.background = '#F2F2F2';
+        TXTcontent.innerHTML="Background: #F2F2F2";
     } else if (document.body.style.background = '#F2F2F2'){ 
         document.body.style.background = '#C7C7C7';
+        TXTcontent.innerHTML="Background: #C7C7C7";
     } else {
         document.body.style.background = '#292929';
+        TXTcontent.innerHTML="Background: #292929";
     }
 }
