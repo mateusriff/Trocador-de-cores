248566ec38db95e620bd3521890da726c8fda6df
mateusriff <mateusnriff@gmail.com>
2022-05-24 15:51:40 -0300

    second commit

---
+++ main.js
@@ -0,0 +1,11 @@
+//teste
+
+function changecolor (){
+    if(document.body.style.background = '#292929'){
+         document.body.style.background = '#F2F2F2';
+    } else if (document.body.style.background = '#F2F2F2'){ 
+        document.body.style.background = '#C7C7C7';
+    } else {
+        document.body.style.background = '#292929';
+    }
+}
