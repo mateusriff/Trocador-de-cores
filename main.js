8e31153ca6e5e895e0ea38dbe7b144e72a3cb2d6
mateusriff <mateusnriff@gmail.com>
2022-05-24 18:24:05 -0300

    Bug fixes. Now background color and HEX text change as they should.

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
-//teste
+const element = document.querySelector('.black');
+
+var color = 'black';
+
+//element.addEventListener('click', () => {
+//  element.classList.toggle('white');
+//});
 
 function changecolor (){
-    if(document.body.style.background = '#292929'){
+    if (color === 'black'){
         document.body.style.background = '#F2F2F2';
         TXTcontent.innerHTML="Background: #F2F2F2";
-    } else if (document.body.style.background = '#F2F2F2'){ 
+        color = 'white';
+    } else if (color === 'white'){ 
         document.body.style.background = '#C7C7C7';
         TXTcontent.innerHTML="Background: #C7C7C7";
-    } else {
+        color = 'gray';
+    } else if (color === 'gray'){
         document.body.style.background = '#292929';
         TXTcontent.innerHTML="Background: #292929";
+        color = 'black';
     }
 }
